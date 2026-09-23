5b0b87fc108250686ca99459c3464fb2e2191e18
adrysantos363-art <adrysantos363@gmail.com>
2026-09-23 13:48:27 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -13,3 +13,14 @@ window.TH_FIREBASE_CONFIG = {
   appId: "1:305227434742:web:ab2ee16d85db2861f5fad7",
   measurementId: "G-0P0T4TKS66"
 };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBbm37gVOWlnJGWrGtLmiRa8NjrjsdMvG0",
+  authDomain: "thimports-1ce81.firebaseapp.com",
+  databaseURL: "https://thimports-1ce81-default-rtdb.firebaseio.com",
+  projectId: "thimports-1ce81",
+  storageBucket: "thimports-1ce81.firebasestorage.app",
+  messagingSenderId: "305227434742",
+  appId: "1:305227434742:web:ab2ee16d85db2861f5fad7",
+  measurementId: "G-0P0T4TKS66"
+};
